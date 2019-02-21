f06c3ff1b64a2362e868ba72c80466b3cef07390
miguel.fernandez <miguel.fernandez@riskms.es>
2019-02-21 15:38:12 +0100

    commit

---
+++ src/main/java/g/Main.java
@@ -0,0 +1,13 @@
+package g;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Persona p = new Persona();
+		
+		System.out.println("hola "+p.getNombre());
+
+	}
+
+}
---
+++ src/main/java/g/Persona.java
@@ -0,0 +1,29 @@
+package g;
+
+public class Persona {
+
+	private String nombre;
+	private String apellido;
+	private String dire;
+	
+	public String getNombre() {
+		return "Miguel Angel";
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDire() {
+		return dire;
+	}
+	public void setDire(String dire) {
+		this.dire = dire;
+	}
+	
+	
+}
