93d81e035b1d3d1b3869af7e43ab3b1d3e1ca700
miguel.fernandez <miguel.fernandez@riskms.es>
2019-02-21 16:00:29 +0100

    insertando logger

--- src/main/java/g/Main.java
+++ src/main/java/g/Main.java
@@ -1,12 +1,16 @@
 package g;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class Main {
-
+	
+	
 	public static void main(String[] args) {
-		
+		Logger log = (Logger) Logger.getInstance(Main.class);
+		BasicConfigurator.configure();
 		Persona p = new Persona();
 		
-		System.out.println("hola "+p.getNombre());
+		log.info("Bienvenido "+p.getNombre());
 
 	}
 
